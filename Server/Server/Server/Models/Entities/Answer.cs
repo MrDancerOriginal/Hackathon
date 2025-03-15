5f4bb2ffe9c889d60a383bf333d657c2fa41a89c
Maria Lupiak <maria.lupiak@gmail.com>
2025-03-15 11:15:59 +0200

    add models, connect to db

---
+++ Server/Server/Server/Models/Entities/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models.Entities
+{
+    public class Answer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Question")]
+        public int QuestionId { get; set; }
+        public virtual Question Question { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        // Позначення, чи є відповідь правильною
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ Server/Server/Server/Models/Entities/PdfFile.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Models.Entities
+{
+    public class PdfFile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        // Назва файлу, як завантажено користувачем
+        [Required]
+        public string FileName { get; set; }
+
+        // Шлях до файлу у файловій системі (якщо зберігаємо його на диску)
+        public string FilePath { get; set; }
+
+        // Текст, витягнутий із PDF (конвертований для подальшого запиту до LLM)
+        public string ExtractedText { get; set; }
+
+        public DateTime UploadedDate { get; set; } = DateTime.UtcNow;
+    }
+}
